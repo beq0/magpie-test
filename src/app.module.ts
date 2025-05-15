b6821555b82167160eea151ebab5d783314114cb
beq0 <gugulashvili.beka@gmail.com>
2025-05-15 20:39:55 +0400

    feat: add dotenv config

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,17 @@ import { AppService } from './app.service';
 import { PoolsModule } from './modules/pools/pools.module';
 import { TicksModule } from './modules/ticks/ticks.module';
 import { TokensModule } from './modules/tokens/tokens.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [PoolsModule, TicksModule, TokensModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    PoolsModule,
+    TicksModule,
+    TokensModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
